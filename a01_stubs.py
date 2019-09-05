f3b2be23c2f40d3804f6b75dde1f4fff9f94c1d9
garrettz <zyzyzygarrett@gmail.com>
2019-09-04 22:22:29 -0400

    I added code about Chinese Zodiac signs to the a01_stubs.py
    I manipulated the code in the t00_chocolate.py
    
    I added a repo link, google docs link, and I acknowledged that Shawn Villahermosa helped me with this assignment.

--- a01_stubs.py
+++ a01_stubs.py
@@ -1,6 +1,6 @@
 ######################################################################
-# Author: Dr. Scott Heggen      TODO: Change this to your name
-# Username: heggens             TODO: Change this to your username
+# Author: Zyshavia Garrett
+# Username: garrettz
 #
 # Assignment: A01
 #
@@ -16,10 +16,72 @@
 
 ######################################################################
 # (Required) Task 1
-# TODO Ask user for their birth year
+
+import time
+user_input = (input("What is your birth year?"))
+
+if user_input == "2000":
+    print("You are a fire breathing dragon.")
+    print("Hot, Hot!")
+
+elif user_input == "2001":
+    print("You are a slithering snake!")
+    print("The biggest snake of them all!")
+
+elif user_input == "2002":
+    print("You are a happy horse!")
+    print("Such a nobel steed!")
+
+elif user_input == "2003":
+    print("You are a gracious goat!")
+    print("It must be nice being the Greatest Of All Time!")
+    time.sleep(2)
+    print("Get it...because it spells goat:)")
+
+elif user_input == "2004":
+    print("You're a magical monkey!")
+    time.sleep(2)
+    print("How is the view from the trees?")
+
+elif user_input == "2005":
+    print("You are a righteous rooster!")
+    time.sleep(2)
+    print("Hmmmmmm...fried chicken!")
+
+elif user_input == "2006":
+    print("You are a dog!")
+    print("Who's a good boy!")
+    time.sleep(2)
+    print("who let the dogs out!")
+
+elif user_input == "2007":
+    print("You are a plump pig!")
+    time.sleep(2)
+    print("Oink, oink!")
+
+elif user_input == "2008":
+    print("You are a rat!")
+    print("Ewwwww...I'm just kidding. Rats are cute!")
+    time.sleep(2)
+    print("Sometimes...")
+
+elif user_input == "2009":
+    print("You are a Ox!")
+    time.sleep(2)
+    print("What an odd but beautiful animal to be!")
+
+elif user_input == "2010":
+    print("You're a Tiger!")
+    time.sleep(2)
+    print("It's the eye of the tiger! :) ")
+
+elif user_input == "2011":
+    print("You are a cute and fuzzy rabbit!")
+    time.sleep(2)
+    print("Which is my favorite animal by the way!")
 
 
-# TODO Check the year using if conditionals, and print the correct animal for that year.
+print('')
 # See the a01_pets.py for examples
 
 
